6b4005e706f73486e43819c26f5051f2c0435650
ReethikaVenturi <110443993+ReethikaVenturi@users.noreply.github.com>
2024-06-15 15:54:50 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -27,7 +27,7 @@ const SearchBar = () => {
         border: '1px solid #e3e3e3',
         pl: 2,
         boxShadow: 'none',
-        mr: { sm: 140 },
+        mr: { sm: 10 },
         
       }}
     >
